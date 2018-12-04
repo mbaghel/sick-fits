eb4439fb4c14aac8c4899eba89b0f0c8cbe7ae2a
mbaghel <michael.baghel@gmail.com>
2018-12-04 22:21:49 +0400

    Chapter 26

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
-// TODO: use express middleware to handle cookies (JWT)
-// TODO: use express middleware to populate current user 
+server.express.use(cookieParser());
+// TODO: use express middleware to populate current user
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL    
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`server started at http://localhost:${deets.port}`);
   }
-}, deets => {
-  console.log(`server started at http://localhost:${deets.port}`)
-})
+);
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -37,6 +40,32 @@ const Mutation = {
     // TODO
     // 3. Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // set emails to lowercase (so it'll always match when they login)
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    // create JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set jwt as cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return user to the browser
+    return user;
   }
 };
 
